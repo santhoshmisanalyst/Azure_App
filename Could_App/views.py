bb0697394f114c5bee369a2345a67e96232a4b21
santhoshmisanalyst@gmail.com <santhoshmisanalyst@gmail.com>
2024-01-27 00:03:39 +0300

    Change View

--- Could_App/views.py
+++ Could_App/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def members(request):
-    return HttpResponse("Hello world! Santhosh Goud")
+    return HttpResponse("Hello world! Bolgam Santhosh Goud ")
