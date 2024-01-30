8b0db4d789995ce6d692615e80d0c1a4204d1bb3
santhoshmisanalyst@gmail.com <santhoshmisanalyst@gmail.com>
2024-01-30 15:07:18 +0300

    Add html pages

--- Could_App/views.py
+++ Could_App/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def members(request):
-    return HttpResponse("Hello world! Bolgam Santhosh Goud and Sadhika ")
+    
+    return render(request, 'Login.html')
+    #return HttpResponse("Hello world! Bolgam Santhosh Goud and Sadhika ")
